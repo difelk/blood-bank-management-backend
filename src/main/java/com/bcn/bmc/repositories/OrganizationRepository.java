0e55174ad97fe178725b1d29ac538a3280672f02
difelk <dilshan.f@theavo.net>
2024-09-07 23:59:41 +0530

    authorization v01

--- src/main/java/com/bcn/bmc/repositories/OrganizationRepository.java
+++ src/main/java/com/bcn/bmc/repositories/OrganizationRepository.java
@@ -1,7 +1,15 @@
 package com.bcn.bmc.repositories;
 
+import com.bcn.bmc.models.Address;
 import com.bcn.bmc.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+
+    @Query("select o from Organization o where o.id = ?1")
+    Optional<Organization> findOrganizationById(@Param("id") int id);
 }
